d0b56fd2bad1980cf6a72c3207f7755e8b299a7a
Enger Jimenez <ejimenez@oriontek.do>
2020-03-21 18:54:12 -0400

    First commit

--- web-front/src/store/index.js
+++ web-front/src/store/index.js
@@ -1,14 +1,51 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+import urls from './urls';
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    urls,
+    credentials: {
+      username: 'admin',
+      password: '123'
+    },
+    user: {
+      id: 1,
+      username: 'admin',
+      password: '123',
+      logged: true
+    }
   },
   mutations: {
+    LOGIN(state) {
+      state.user.logged = true;
+    },
+    LOGOUT(state) {
+      state.user.logged = false;
+    },
   },
   actions: {
+    login(state, payload) {
+      if (payload === true) {
+        state.commit('LOGIN');
+      }
+    },
+    logout(state, payload) {
+      if (payload === false) {
+        state.commit('LOGOUT');
+      }
+    }
+  },
+  getters: {
+    isUserLogged(state) {
+      return state.user.logged;
+    },
+    user(state) {
+      return state.user;
+    }
   },
   modules: {
   }
---
+++ web-front/src/store/urls.js
@@ -0,0 +1,3 @@
+export default {
+  server: 'https://cors-anywhere.herokuapp.com/http://street-control-api.herokuapp.com'
+}
